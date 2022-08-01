517dfc83ca8852d13f9025bad200c8bb6f6da150
tianyou.lan <tianyou.lan@jiduauto.com>
2022-08-01 15:03:51 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AutoUpdateGithubHosts
+
+## 简介
+
 Auto update github hosts
+
+## 环境
+
+Python: 3.8.9
+
+## 使用方法
+
+```code=shell
+sudo chmod 777 /etc/hosts
+python3 update_github_hosts.py
+```
--- update_github_hosts.py
+++ update_github_hosts.py
@@ -4,16 +4,20 @@ import logging
 import difflib
 from pprint import pprint
 
-logging.basicConfig(level=logging.DEBUG)
+desktop_path = "/Users/tianyou.lan/Desktop"
+logging.basicConfig(
+    filename=f'{desktop_path}/update_github_hosts.log',
+    format='%(asctime)s %(message)s',
+    level=logging.DEBUG)
 host_file = "/etc/hosts"
 
 
-def update_hosts_file_github_content():
+def update_github_hosts():
     """
     According to the resolution: https://github.com/Licoy/fetch-github-hosts
     :return:
     """
-    r = requests.get("http://hosts.gitcdn.top/hosts.txt")
+    r = requests.get("https://hosts.gitcdn.top/hosts.txt")
     with open(host_file, "r") as rf:
         content = rf.read()
         new_content = re.sub(r"# fetch-github-hosts begin(.|\n)+# fetch-github-hosts end", r.text, content)
@@ -26,6 +30,7 @@ def update_hosts_file_github_content():
     d = difflib.Differ()
     diff = d.compare(content, new_content)
     pprint(list(diff))
+    logging.info("changes updated")
     with open(host_file, "w") as wf:
         wf.write(new_content.strip("/n"))
     with open(host_file, "r") as rnf:
@@ -33,4 +38,4 @@ def update_hosts_file_github_content():
 
 
 if __name__ == '__main__':
-    update_hosts_file_github_content()
+    update_github_hosts()
